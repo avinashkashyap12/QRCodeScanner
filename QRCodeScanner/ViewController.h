7a8f91873e5280f332b2491530741d777531e21c
Avinash Kashyap <avinash@headerlabs.com>
2016-08-20 14:31:02 +0530

    Initial Commit

---
+++ QRCodeScanner/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  QRCodeScanner
+//
+//  Created by Avinash Kashyap on 8/20/16.
+//  Copyright © 2016 Headerlabs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
